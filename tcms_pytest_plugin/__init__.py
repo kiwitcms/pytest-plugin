6fed932899cfccabf8dff9b8f98e8476cb78c1b3
Alexander Todorov <atodorov@mrsenko.com>
2021-12-07 20:50:10 +0200

    Update status reporting b/c of the 3-tier callback from pytest
    pytest_report_teststatus is called 3 times for setup/call/teardown
    and it is not very convenient to figure out the overall status.

--- tcms_pytest_plugin/__init__.py
+++ tcms_pytest_plugin/__init__.py
@@ -38,26 +38,41 @@ class KiwiTCMSPlugin:
     backend = Backend(prefix='[pytest]')
     executions = []
     status_id = 0
+    status_weight = 0
     comment = ''
 
     def pytest_runtestloop(self, session):
         self.backend.configure()
 
     def pytest_runtest_logstart(self, nodeid, location):
+        self.status_weight = 0
+        self.comment = ''
+
         test_case, _ = self.backend.test_case_get_or_create(nodeid)
         self.backend.add_test_case_to_plan(test_case['id'], self.backend.plan_id)
         self.executions = self.backend.add_test_case_to_run(test_case['id'], self.backend.run_id)
 
     @pytest.hookimpl(hookwrapper=True)
     def pytest_report_teststatus(self, report, config):
-        yield
-        if report.when == 'teardown':
-            if report.outcome == 'passed':
+        outcome = yield
+
+        # this hooks is executed 3 times for setup/call/teardown each of which
+        # has their own status. Thus a positive status cannot override a negative one
+        # Positive statuses are considered only in the beginning
+        if report.when in ['setup', 'call']:
+            if self.status_weight >=0 and report.outcome == 'passed':
                 self.status_id = self.backend.get_status_id('PASSED')
-            elif report.outcome == 'failed':
-                self.status_id = self.backend.get_status_id('FAILED')
-            elif report.outcome == 'skipped':
+                self.status_weight = 1
+            elif self.status_weight >=0 and report.outcome == 'skipped':
                 self.status_id = self.backend.get_status_id('WAIVED')
+                self.status_weight = 1
+
+        # a negative status is stronger, will always override any other status
+        # negative statuses are considered in every occasion
+        if report.outcome in ['error', 'failed']:
+            self.status_id = self.backend.get_status_id(report.outcome.upper())
+            self.status_weight = -1
+            self.comment = f"<pre>\n{report.longrepr}\n</pre>"
 
     def pytest_runtest_logfinish(self, nodeid, location):
         for execution in self.executions:
