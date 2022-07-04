d079cacf28f293539d045278e3a998755a3e1675
Alexander Todorov <atodorov@mrsenko.com>
2022-07-04 21:57:20 +0300

    Encapsulate Backend() into the plugin class
    and make use of annotation information from latest tcms-api version

--- tcms_pytest_plugin/__init__.py
+++ tcms_pytest_plugin/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Copyright (c) 2019 Dmitry Dygalo <dadygalo@gmail.com>
 # Copyright (c) 2021 Bryan Mutai <mutaiwork@gmail.com>
-# Copyright (c) 2021 Alexander Todorov <atodorov@otb.bg>
+# Copyright (c) 2021-2022 Alexander Todorov <atodorov@otb.bg>
 #
 # Licensed under the GPLv3: https://www.gnu.org/licenses/gpl.html
 #
@@ -9,9 +9,9 @@
 
 
 import pytest
-from tcms_api.plugin_helpers import Backend
+from tcms_api import plugin_helpers
 
-backend = Backend(prefix='[pytest]')
+from .version import __version__
 
 
 def pytest_addoption(parser):
@@ -29,35 +29,41 @@ def pytest_configure(config):
         )
 
 
+class Backend(plugin_helpers.Backend):
+    name = "kiwitcms-pytest-plugin"
+    version = __version__
+
+
 class KiwiTCMSPlugin:
+    backend = Backend(prefix='[pytest]')
     executions = []
     status_id = 0
     comment = ''
 
     def pytest_runtestloop(self, session):
-        backend.configure()
+        self.backend.configure()
 
     def pytest_runtest_logstart(self, nodeid, location):
-        test_case, _ = backend.test_case_get_or_create(nodeid)
-        backend.add_test_case_to_plan(test_case['id'], backend.plan_id)
-        self.executions = backend.add_test_case_to_run(test_case['id'], backend.run_id)
+        test_case, _ = self.backend.test_case_get_or_create(nodeid)
+        self.backend.add_test_case_to_plan(test_case['id'], self.backend.plan_id)
+        self.executions = self.backend.add_test_case_to_run(test_case['id'], self.backend.run_id)
 
     @pytest.hookimpl(hookwrapper=True)
     def pytest_report_teststatus(self, report, config):
         yield
         if report.when == 'teardown':
             if report.outcome == 'passed':
-                self.status_id = backend.get_status_id('PASSED')
+                self.status_id = self.backend.get_status_id('PASSED')
             elif report.outcome == 'failed':
-                self.status_id = backend.get_status_id('FAILED')
+                self.status_id = self.backend.get_status_id('FAILED')
             elif report.outcome == 'skipped':
-                self.status_id = backend.get_status_id('WAIVED')
+                self.status_id = self.backend.get_status_id('WAIVED')
 
     def pytest_runtest_logfinish(self, nodeid, location):
         for execution in self.executions:
-            backend.update_test_execution(execution["id"], self.status_id, self.comment)
+            self.backend.update_test_execution(execution["id"], self.status_id, self.comment)
 
     @pytest.hookimpl(hookwrapper=True)
     def pytest_terminal_summary(self, terminalreporter, exitstatus, config):
         yield
-        backend.finish_test_run()
+        self.backend.finish_test_run()
